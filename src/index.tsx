14655d7596e4eb2647dfe02338015b17c5a90c56
kamazim <kainamazim@gmail.com>
2023-01-14 01:01:39 -0300

    feat: add base style files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import './styles/normalize.css';
+import './styles/index.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
